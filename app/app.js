ea2084b1cdbca889058ea7b08a2fe8bd1f8f0d5d
thangnc <ncthang198@gmail.com>
2019-05-18 16:52:46 +0700

    thangnct

--- app/app.js
+++ app/app.js
@@ -21,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//
+//thangnct
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
