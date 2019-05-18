25ca0e6533c5c4fe59e02120c99ed5fd406898c5
thangnc <ncthang198@gmail.com>
2019-05-18 16:40:55 +0700

    ok

---
+++ app/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    server: {
+        url: "localhost",
+        port: '1999'
+    },
+    mongo: "mongodb://localhost:27017/socket_io",
+    socket: {
+        port: "2000"
+    }
+};
